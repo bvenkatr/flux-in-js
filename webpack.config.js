7beacd620c468c535ef6aabb62b96b8a548a9b80
venkat <bvenkatr2@gmail.com>
2017-03-04 12:51:44 +0530

    add increment and decrement buttons

--- webpack.config.js
+++ webpack.config.js
@@ -20,5 +20,13 @@ module.exports = {
                 presets: ["es2015", "react"]
             }
         }]
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    ]
 };
